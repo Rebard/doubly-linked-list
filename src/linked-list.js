d3dd7113d9f82564b0a29b01a77a917ae80b30fc
Rebard <31970194+Rebard@users.noreply.github.com>
2017-09-24 22:57:33 +0300

    Add files via upload

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,175 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+	
+    constructor() {
+		this._head = null;
+		this._tail = null;
+		this.length = 0;
+	}
 
-    append(data) {}
+    append(data) {
+		var temp = new Node(data);
+		if(this._head == null)
+		{
+			temp.next = temp.prev = null;
+			this._head = this._tail = temp;
+		}
+		else{
+			temp.next = null;
+			temp.prev = this._tail;
+			this._tail.next = temp;
+			this._tail = temp;
+		}
+		this.length++;
+		return this;
+	}
 
-    head() {}
+    head() {
+		if(this.length === 0)
+			return null;
+		else
+			return this._head.data;
+	}
 
-    tail() {}
+    tail() {
+		if(this.length === 0)
+			return null;
+		else
+			return this._tail.data;
+	}
 
-    at(index) {}
+    at(index) {
+		var temp = this._head;
+		if((index > this.length - 1) || (index < 0))
+		{
+			return;
+		}
+		for(var i = 0;i < index;i++)
+		{
+			temp = temp.next;
+		}
+		return temp.data;
+	}
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+		var temp = this._head, temp1 = null;
+		var insert_temp = new Node(data);
+		if(index > this.length - 1)
+		{
+			return this;
+		}
+		if(index === 0)
+		{
+			this._head.prev = insert_temp;
+			insert_temp.next = this._head;
+			insert_temp.prev = null;
+			this._head = insert_temp;
+		}
+		else{
+			for(var i = 0;i < index;i++)
+			{
+				temp = temp.next;
+			}
+			//temp1 = temp.prev;
+			temp.prev.next = insert_temp;
+			temp.prev = insert_temp;
+			insert_temp.prev = temp.prev;
+			insert_temp.next = temp;
+		}
+		return this;
+	}
 
-    isEmpty() {}
+    isEmpty() {
+		if(this.length === 0)
+			return true;
+		else 
+			return false;
+	}
 
-    clear() {}
+    clear() {
+		if(this.length === 0 )
+			return this;
+		var temp = this._head, temp1 = null;
+		while(this.length > 0 )
+		{
+			this._head = temp.next;
+			temp1 = temp;
+			temp = null;
+			this.length--;
+			temp = temp1.next;
+		}
+	this._tail = this._head = null;
+	return this;
+	}
 
-    deleteAt(index) {}
+    deleteAt(index) {
+		if(index < 0 || index > (this.length - 1) )
+			return this;
+		var temp = null;
+		
+		if(index === 0)
+		{
+			if(this.length === 1 && index == 0)
+			{
+				this._head = this._tail = null;
+				this.length--;
+				return this;	
+			}
+			temp = this._head;
+			temp = temp.next;
+			temp.prev = null;
+			this._head = temp;
+			this.length--;
+			return this;	
+		}
+		if(index === this.length - 1)
+		{
+			temp = this._tail;
+			temp = temp.prev;
+			temp.next = null;
+			this._tail = temp;
+		}
+		else
+		{
+			temp = this._head;
+			for(var i = 0; i < index; i++)
+				temp = temp.next;
+			temp.prev.next = temp.next;
+			temp.next.prev = temp.prev;
+			temp = temp.next = temp.prev = null;
+		}
+		this.length--;
+		return this;
+	}
 
-    reverse() {}
+    reverse() {
+		var temp = this._head, temp1 = null;
+		while(temp)
+		{
+			temp1 = temp.next;
+			temp.next = temp.prev;
+			temp.prev = temp1;
+			temp = temp.prev;
+		}
+		temp1 = this._head;
+		this._head = this._tail;
+		this._tail = temp1;
+		return this;
+	}
 
-    indexOf(data) {}
+    indexOf(data) {
+		var temp = this._head;
+		var index = 0;
+		while(temp)
+		{
+			if(temp.data === data)
+				return index;
+			index++;
+			temp = temp.next;
+		}
+		return -1;
+	}
 }
 
 module.exports = LinkedList;
